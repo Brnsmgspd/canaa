748ff108dd5a0135f2da7484817467465c724d6a
Brnsmgspd <101212470+Brnsmgspd@users.noreply.github.com>
2023-06-07 17:20:15 -0300

    Delete Site Canaã directory

--- Site Canaã/main.js
+++
@@ -1,6 +0,0 @@
-// const myHeading = document.querySelector("h1");
-// myHeading.textContent = "Comunidade Cristã Canaã";
-
-// document.querySelector("html").addEventListener("click", function (){
-//     alert("Ouch");
-// });
